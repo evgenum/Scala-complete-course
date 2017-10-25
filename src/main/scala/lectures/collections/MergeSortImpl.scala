2ffd863d725e5ed9ab4b43ac84529e435602122a
e.malyukov <malukovevgeny@ya.ru>
2017-10-25 13:55:39 +0300

    Complete collection.MergeSortImpl

--- src/main/scala/lectures/collections/MergeSortImpl.scala
+++ src/main/scala/lectures/collections/MergeSortImpl.scala
@@ -8,6 +8,19 @@ package lectures.collections
   */
 object MergeSortImpl extends App {
 
-  def mergeSort(data: Seq[Int]): Seq[Int] = data
+  def mergeSort(data: Seq[Int]): Seq[Int] = {
+    if(data.isEmpty) data
+    else if(data.tail.isEmpty) data
+    else {
+      val a = data.splitAt(data.length / 2)
+      merge(mergeSort(a._1),mergeSort(a._2))
+    }
+  }
 
+  def merge(a: (Seq[Int],Seq[Int])): Seq[Int] = a match {
+    case (Seq(),right) => right
+    case (left,Seq()) => left
+    case (left,right) if left.head > right.head => Seq(right.head) ++ merge(left, right.tail)
+    case (left,right) if left.head <= right.head => Seq(left.head) ++ merge(left.tail, right)
+  }
 }
